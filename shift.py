25ea28bb700fcacb7d964a874c48ea4e9d878ffc
Aaron Burton <81588424+aburton-marketing@users.noreply.github.com>
2021-06-12 19:28:32 -0700

    Update shift.py

--- shift.py
+++ shift.py
@@ -1,13 +1,26 @@
-nums = [0,1,2,10,0,0]
-insert = 20
+# array right shift that maintains order
+# copyright 2020 aaron burton
+
+
+nums = [20,0,1,2,10,0]
+insert = 30
 pos = 0
-def shift(nums, pos, insert):
-  i = len(nums) / 2
-  while True:
-    if i == 0:
-      break
-    else:
-      nums[i + 1], nums[i] = nums[i], nums[i + 1]
-    i -= 1
-  nums[pos] = insert
-  return nums
+
+def aShift(nums, pos, insert):
+	i = len(nums) - 2
+	while i >= pos:
+		print(pos, i, nums)
+		nums[i + 1], nums[i] = nums[i], nums[i + 1]
+		i -= 1
+	nums[pos] = insert
+	return nums
+
+z = 0
+while True:
+	if nums[z] == 0:
+		pos = z + 1
+		aShift(nums, pos, 0)
+		z += 1
+	z += 1
+	if z >= len(nums)-1:
+		break
